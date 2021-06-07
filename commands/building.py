b1aaa813e9afc8900b1a1441e74e6aad88aeec64
John W <telestrial@gmail.com>
2021-06-07 18:44:04 -0400

    Building Menu Contribution

---
+++ commands/building.py
@@ -0,0 +1,121 @@
+from evennia.contrib.building_menu import BuildingMenu
+from commands.command import Command
+
+class EditCmd(Command):
+
+    """
+    Editing command.
+
+    Usage:
+      @edit [object]
+
+    Open a building menu to edit the specified object.  This menu allows to
+    specific information about this object.
+
+    Examples:
+      @edit here
+      @edit self
+      @edit #142
+
+    """
+
+    key = "@edit"
+    locks = "cmd:id(1) or perm(Builders)"
+    help_category = "Building"
+
+    def func(self):
+        if not self.args.strip():
+            self.msg("|rYou should provide an argument to this function: the object to edit.|n")
+            return
+
+        obj = self.caller.search(self.args.strip(), global_search=True)
+        if not obj:
+            return
+
+        if obj.typename == "Room":
+            Menu = RoomBuildingMenu
+        else:
+            self.msg("|rThe object {} cannot be edited.|n".format(obj.get_display_name(self.caller)))
+            return
+
+        menu = Menu(self.caller, obj)
+        menu.open()
+
+
+
+# Our building menus
+class RoomBuildingMenu(BuildingMenu):
+
+    """
+    Building menu to edit a room.
+    """
+
+    def init(self, room):
+        self.add_choice("title", key="t", attr="key", glance="{obj.key}", text="""
+                -------------------------------------------------------------------------------
+                Editing the title of {{obj.key}}(#{{obj.id}})
+
+                You can change the title simply by entering it.
+                Use |y{back}|n to go back to the main menu.
+
+                Current title: |c{{obj.key}}|n
+        """.format(back="|n or |y".join(self.keys_go_back)))
+        self.add_choice_edit("description", "d")
+        self.add_choice("exits", "e", glance=glance_exits, text=text_exits, on_nomatch=nomatch_exits)
+
+
+# Menu functions
+def glance_exits(room):
+    """Show the room exits."""
+    if room.exits:
+        glance = ""
+        for exit in room.exits:
+            glance += "\n  |y{exit}|n".format(exit=exit.key)
+
+        return glance
+
+    return "\n  |gNo exit yet|n"
+
+def text_exits(caller, room):
+    """Show the room exits in the choice itself."""
+    text = "-" * 79
+    text += "\n\nRoom exits:"
+    text += "\n Use |y@c|n to create a new exit."
+    text += "\n\nExisting exits:"
+    if room.exits:
+        for exit in room.exits:
+            text += "\n  |y@e {exit}|n".format(exit=exit.key)
+            if exit.aliases.all():
+                text += " (|y{aliases}|n)".format(aliases="|n, |y".join(
+                        alias for alias in exit.aliases.all()))
+            if exit.destination:
+                text += " toward {destination}".format(destination=exit.get_display_name(caller))
+    else:
+        text += "\n\n |gNo exit has yet been defined.|n"
+
+    return text
+
+def nomatch_exits(menu, caller, room, string):
+    """
+    The user typed something in the list of exits.  Maybe an exit name?
+    """
+    string = string[3:]
+    exit = caller.search(string, candidates=room.exits)
+    if exit is None:
+        return
+
+    # Open a sub-menu, using nested keys
+    caller.msg("Editing: {}".format(exit.key))
+    menu.open_submenu("commands.building.ExitBuildingMenu", exit, parent_keys=["e"])
+    return False
+
+class ExitBuildingMenu(BuildingMenu):
+
+    """
+    Building menu to edit an exit.
+
+    """
+
+    def init(self, exit):
+        self.add_choice("key", key="k", attr="key", glance="{obj.key}")
+        self.add_choice_edit("description", "d")
--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -16,6 +16,8 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 
 from evennia import default_cmds
 
+from commands.building import EditCmd
+
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
     """
@@ -34,6 +36,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         #
         # any commands you add below will overload the default ones.
         #
+        self.add(EditCmd())
 
 
 class AccountCmdSet(default_cmds.AccountCmdSet):
